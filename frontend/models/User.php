0c6b0f8c9592819601297d5336f27f254d78cddb
rinadwi <35626791+rinadwi@users.noreply.github.com>
2018-01-23 09:30:38 +0700

    rina-advanced
    tugas UAS PWL

---
+++ frontend/models/User.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace frontend\models;
+use Yii;
+use common\models\User as User1;
+class User extends User1{
+
+}
+?>
---
+++ frontend/views/site/oke.php
@@ -0,0 +1,10 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Tentang';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+oke boss
